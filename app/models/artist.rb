1c9c4ef9e2d73355269e25851fa234c94ec9fd21
Zac Willmington <zac.willmington@gmail.com>
2018-04-26 18:34:36 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,7 +4,6 @@ class Artist < ActiveRecord::Base
 
   def get_genre_of_first_song
     #return the genre of the artist's first saved song
-    binding.pry
     first = Song.find_by(1)
     first.genre
   end
