6ab04d9911dc3c296578ce4ab17e34cb022f6890
Zac Willmington <zac.willmington@gmail.com>
2018-04-26 18:39:55 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,6 +14,7 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
+    binding.pry
     self.artists.all.name
   end
 end
